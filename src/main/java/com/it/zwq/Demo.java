c863ce442a24ebc23ffd51667ac51040e09fe659
Zhou <666666@qq.com>
2022-10-25 12:24:43 +0800

    second

--- src/main/java/com/it/zwq/Demo.java
+++ src/main/java/com/it/zwq/Demo.java
@@ -4,5 +4,7 @@ public class Demo {
     public static void main(String[] args) {
         int a = 1;
         System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
     }
 }
